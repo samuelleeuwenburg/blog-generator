d36b3572b1e9eb8d0d540774f4d40b173bbbb82e
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2020-02-21 15:38:38 +0100

    simple parsing and generation

---
+++ blog/posts/example/content.md
@@ -0,0 +1,6 @@
+# This is an example post
+
+Cras id vulputate lectus. Proin fermentum nec augue vitae pulvinar. Ut id dignissim justo, at mattis erat. Praesent pellentesque, enim a sodales lacinia, ligula quam euismod neque, quis faucibus sapien arcu eu ipsum. Nulla ultricies justo leo, id malesuada erat sollicitudin nec.
+
+## Ut volutpat nec dui non mattis.
+Phasellus consectetur mattis leo, id feugiat eros pharetra nec. Maecenas maximus pulvinar purus et egestas. Duis bibendum felis tortor, vel tincidunt felis bibendum vel.
---
+++ src/main.rs
@@ -0,0 +1,166 @@
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+use regex::Regex;
+use comrak::{markdown_to_html, ComrakOptions};
+
+struct Config {
+    src: String,
+    dest: String,
+    title: String,
+    selector_content: String,
+    selector_title: String,
+    selector_css: String,
+}
+
+struct Template {
+    html: String,
+    css: String,
+}
+
+impl Template {
+    fn new(html: &str, css: &str) -> Template {
+        Template {
+            html: String::from(html),
+            css: String::from(css),
+        }
+    }
+
+    fn build_page(&self, config: &Config, content: &str, title: &str) -> String {
+        let html = self.html.to_owned();
+        let html = html.replace(&config.selector_content, &content);
+        let html = html.replace(&config.selector_title, &title);
+        let html = html.replace(&config.selector_css, &self.css);
+
+        html
+    }
+}
+
+struct Post {
+    slug: String,
+    title: String,
+    html: String,
+}
+
+impl Post {
+    fn new(slug: &str, html: &str) -> Post {
+        let re = Regex::new("<h1>(.+)</h1>").unwrap();
+
+        let title = match re.captures(html) {
+            Some(capture) => {
+                String::from(capture.get(0).unwrap().as_str())
+            }
+            None => String::from(slug),
+        };
+
+        Post {
+            title,
+            slug: String::from(slug),
+            html: String::from(html),
+        }
+    }
+
+    fn render_link(&self) -> String {
+        format!("\n<a href=\"/{}\">{}</a>", self.slug, self.title)
+    }
+}
+
+fn open_file(path: &str) -> std::io::Result<String> {
+    let file = File::open(path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut content = String::new();
+    buf_reader.read_to_string(&mut content)?;
+
+    Ok(content)
+}
+
+fn get_posts(config: &Config) -> std::io::Result<Vec<Post>> {
+    println!("reading markdown posts..");
+
+    let files = fs::read_dir(format!("{}/posts", config.src))?;
+    let mut posts: Vec<Post> = Vec::new();
+
+    for entry in files {
+        let dir = entry?;
+        let metadata = dir.metadata()?;
+
+        if metadata.is_dir() {
+            let mut path = dir.path().clone();
+            path.push("content.md");
+
+            let markdown = open_file(path.to_str().unwrap())?;
+            let html = markdown_to_html(&markdown, &ComrakOptions::default());
+            let slug = dir.file_name().to_str().unwrap().to_owned();
+
+            posts.push(Post::new(&slug, &html));
+        }
+    }
+
+    Ok(posts)
+}
+
+fn get_template(config: &Config) -> std::io::Result<Template> {
+    println!("reading template..");
+
+    let html = open_file(&format!("{}/template.html", config.src))?;
+    let css = open_file(&format!("{}/style.css", config.src))?;
+
+    Ok(Template::new(&html, &css))
+}
+
+fn write_html_files(config: &Config, template: &Template, posts: &Vec<Post>) -> std::io::Result<()> {
+    let path = Path::new(&config.dest);
+    match fs::create_dir(path) {
+        Ok(_) => println!("creating folder.."),
+        Err(_) => {
+            println!("destination folder already exists, cleaning up..");
+            fs::remove_dir_all(path)?;
+            println!("creating folder..");
+            fs::create_dir(path)?;
+        },
+    }
+
+    println!("creating html files..");
+
+    for post in posts {
+        let file_name = format!("{}.html", post.slug);
+        let file_path = path.clone().join(&file_name);
+        let mut file = File::create(file_path)?;
+
+        file.write_all(template.build_page(&config, &post.html, &post.title).as_bytes())?;
+        println!("-> {}", &file_name);
+    }
+
+    println!("creating index..");
+
+    let index_path = path.clone().join("index.html");
+    let mut file = File::create(index_path)?;
+    let mut html = String::from("");
+
+    for post in posts {
+        html.push_str(&post.render_link());
+    }
+
+    file.write_all(template.build_page(&config, &html, &config.title).as_bytes())?;
+
+    println!("\ndone!");
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let config = Config {
+        src: String::from("./blog"),
+        dest: String::from("./dist"),
+        title: String::from("Blogname"),
+        selector_content: String::from("CONTENT"),
+        selector_title: String::from("TITLE"),
+        selector_css: String::from("CSS"),
+    };
+
+    let posts = get_posts(&config)?;
+    let template = get_template(&config)?;
+
+    write_html_files(&config, &template, &posts)
+}
